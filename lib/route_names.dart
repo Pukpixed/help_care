8c0227b0db2206eb1d38fa3c929ee0caf7a0b7ee
Kaopunnatchaya <boonnak9871@gmail.com>
2026-03-04 15:43:23 +0700

    Update route_names.dart

--- lib/route_names.dart
+++ lib/route_names.dart
@@ -1,6 +1,6 @@
 // lib/route_names.dart
 class RouteNames {
-  // หลัก
+
   static const splash = '/splash';
   static const first = '/first';
   static const auth = '/auth';
@@ -9,15 +9,14 @@ class RouteNames {
   static const editProfile = '/edit-profile';
   static const patients = '/patients';
 
-  // ฟีเจอร์
 
   static const careLog = '/care-log';
   static const sos = '/sos';
 
-  // ✅ ข่าวสาร/ความรู้การดูแล (เพิ่ม)
+
   static const careNews = '/care-news';
 
-  // ✅ แจ้งเตือน
+
   static const notifications = '/notifications';
   static const notificationDetail = '/notification-detail';
   static const medScheduleList = '/med-schedule-list';
